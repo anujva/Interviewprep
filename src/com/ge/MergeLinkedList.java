dfdc0be41720164997f025ddf16288f7a6542386
Anuj Varma <anujva@gmail.com>
2017-05-08 22:55:38 -0700

    Merge two sorted linked list was more complex than I thought. Got accepted though

---
+++ src/com/ge/MergeLinkedList.java
@@ -0,0 +1,89 @@
+package com.ge;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+/**
+ * Created by anuj on 5/8/17.
+ */
+public class MergeLinkedList {
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode temp1 = l1;
+        ListNode temp2 = l2;
+        ListNode head = null;
+        ListNode temp = null;
+        ListNode oldTemp = null;
+        while (temp1 != null || temp2 != null) {
+            if (temp1 != null && temp2 != null) {
+                if (temp1.val < temp2.val) {
+                    if (temp == null) {
+                        temp = new ListNode(temp1.val);
+                    } else {
+                        temp.val = temp1.val;
+                    }
+                    temp1 = temp1 != null ? temp1.next : null;
+                    oldTemp = temp;
+                    temp.next = new ListNode(-1);
+                } else {
+                    if (temp == null) {
+                        temp = new ListNode(temp2.val);
+                    } else {
+                        temp.val = temp2.val;
+                    }
+                    temp2 = temp2 != null ? temp2.next : null;
+                    oldTemp = temp;
+                    temp.next = new ListNode(-1);
+                }
+                if (head == null) {
+                    head = temp;
+                }
+                temp = temp.next;
+            } else if (temp1 == null) {
+                //temp1 is null.. only temp2 left at this point
+                while (temp2 != null) {
+                    if (temp == null) {
+                        temp = new ListNode(temp2.val);
+                    } else {
+                        temp.val = temp2.val;
+                    }
+                    if (head == null) {
+                        head = temp;
+                    }
+                    temp2 = temp2 != null ? temp2.next : null;
+                    oldTemp = temp;
+                    temp.next = new ListNode(-1);
+                    temp = temp.next;
+                }
+            } else if (temp2 == null) {
+                //temp1 is null.. only temp2 left at this point;
+                while (temp1 != null) {
+                    if (temp == null) {
+                        temp = new ListNode(temp1.val);
+                    } else {
+                        temp.val = temp1.val;
+                    }
+                    if (head == null) {
+                        head = temp;
+                    }
+                    temp1 = temp1 != null ? temp1.next : null;
+                    oldTemp = temp;
+                    temp.next = new ListNode(-1);
+                    temp = temp.next;
+                }
+            }
+        }
+        if (oldTemp != null) oldTemp.next = null;
+
+        return head;
+    }
+}
---
+++ src/com/ge/ReverseInteger.java
@@ -0,0 +1,78 @@
+package com.ge;
+
+import java.util.LinkedList;
+
+/**
+ * Created by anuj on 5/6/17.
+ */
+public class ReverseInteger {
+    public static void main(String [] args) {
+        System.out.println(isValid("([)]"));
+    }
+
+    public static int reverse(int n){
+        int index = 0;
+        int reversedNum = 0;
+        while(n != 0){
+            int digit = n %10;
+            n = n/10;
+            if(reversedNum > 0 && ((long)reversedNum)*10 + digit > Integer.MAX_VALUE){
+                return 0;
+            }
+            if(reversedNum < 0 && ((long)reversedNum)*10 + digit < Integer.MIN_VALUE) {
+                return 0;
+            }
+            reversedNum = reversedNum * 10 + digit;
+        }
+        //we have to keep the sign as well.
+        return reversedNum;
+    }
+
+    public static boolean isValid(String s) {
+        LinkedList<Character> linkedList = new LinkedList<>();
+
+        for(int i=0; i < s.length(); i++){
+            Character c = s.charAt(i);
+            if(c == '(' || c == '[' || c == '{'){
+                linkedList.addFirst(s.charAt(i));
+            }
+
+            if(c == ')'){
+                if(linkedList.isEmpty()){
+                    return false;
+                }
+                Character t = linkedList.pop();
+                if(t != '('){
+                    return false;
+                }
+            }
+
+            if(c == ']'){
+                if(linkedList.isEmpty()){
+                    return false;
+                }
+                Character t = linkedList.pop();
+                if(t != '['){
+                    return false;
+                }
+            }
+
+            if(c == '}'){
+                if(linkedList.isEmpty()){
+                    return false;
+                }
+                Character t = linkedList.pop();
+                if(t != '{'){
+                    return false;
+                }
+            }
+        }
+
+        if(linkedList.isEmpty()){
+            return true;
+        }
+
+        return false;
+    }
+
+}
