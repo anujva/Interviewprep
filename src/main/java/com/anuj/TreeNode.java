e24b85d1e7c9fb28e3dc296b232dbc8be329ec90
212574834 <anuj.varma@ge.com>
2017-10-28 16:04:00 -0700

    intermediate check in

---
+++ src/main/java/com/anuj/TreeNode.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 GE. All Rights Reserved.
+ * GE Confidential: Restricted Internal Distribution
+ */
+package com.anuj;
+
+public class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int data, TreeNode left, TreeNode right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ src/main/java/com/anuj/ValidateBST.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016 GE. All Rights Reserved.
+ * GE Confidential: Restricted Internal Distribution
+ */
+package com.anuj;
+
+public class ValidateBST {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean validateBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return false;
+    }
+}
