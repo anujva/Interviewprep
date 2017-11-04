83029df2a72c96f2c8220b1d2b448b9083bc2ff1
212574834 <anuj.varma@ge.com>
2017-11-04 11:19:54 -0700

    Doubly linked list add node at position

---
+++ src/main/java/com/anuj/InsertNodeAtSpePosDoublyLinkedList.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2016 GE. All Rights Reserved.
+ * GE Confidential: Restricted Internal Distribution
+ */
+package com.anuj;
+
+class DoublyLinkedNode {
+    int data;
+    DoublyLinkedNode next;
+    DoublyLinkedNode prev;
+
+    public DoublyLinkedNode(int data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "DoublyLinkedNode{" + "data=" + data + '}';
+    }
+}
+
+public class InsertNodeAtSpePosDoublyLinkedList {
+    public static void main(String[] args) {
+        DoublyLinkedNode head = new DoublyLinkedNode(1);
+        head.next = new DoublyLinkedNode(2);
+        head.next.prev = head;
+
+        head.next.next = new DoublyLinkedNode(3);
+        head.next.next.prev = head.next;
+        head = insertAtPos(head, 4, 4);
+        System.out.println(head);
+        System.out.println(head.next);
+        System.out.println(head.next.next);
+        System.out.println(head.next.next.next);
+        //        System.out.println(head.next.prev);
+        //        System.out.println(head.prev);
+    }
+
+    public static DoublyLinkedNode insertAtPos(DoublyLinkedNode head, int data, int pos) {
+        if (pos < 1) {
+            return head;
+        }
+
+        if (pos == 1) {
+            //insert at head
+            DoublyLinkedNode temp = new DoublyLinkedNode(data);
+            if (head == null) {
+                return temp;
+            }
+            temp.next = head;
+            head.prev = temp;
+            head = temp;
+            return head;
+        }
+
+        int position = 2;
+        DoublyLinkedNode temp = head;
+        while (position++ < pos) {
+            if (temp.next != null) {
+                temp = temp.next;
+            } else {
+                return head;
+            }
+        }
+        DoublyLinkedNode tempNext = temp.next;
+        DoublyLinkedNode newData = new DoublyLinkedNode(data);
+        temp.next = newData;
+        newData.prev = temp;
+        newData.next = tempNext;
+        return head;
+    }
+}
