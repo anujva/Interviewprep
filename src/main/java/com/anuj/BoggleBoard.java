d336b46c35f16af77a378561cc427d5a51871294
Anuj Varma <anujva@gmail.com>
2017-11-24 00:50:17 -0800

    Added some more code to the Boggle board

--- src/main/java/com/anuj/BoggleBoard.java
+++ src/main/java/com/anuj/BoggleBoard.java
@@ -10,7 +10,7 @@ public class BoggleBoard {
             for (int j = 0; j < board[0].length; j++) {
                 if (word.charAt(0) == board[i][j]) {
                     //begin the search
-                    boolean found = searchForWord(board, word, i, i);
+                    boolean found = searchForWord(board, word, i, j, 1);
                     if (found) return true;
                 }
             }
@@ -18,8 +18,21 @@ public class BoggleBoard {
         return false;
     }
 
-    public static boolean searchForWord(char[][] board, String word, int i, int j) {
+    public static boolean searchForWord(char[][] board, String word, int i,
+                                        int j, int wordIndex) {
+        if (wordIndex < word.length()) {
+            if (i > -1 && i < board.length - 1 && j > -1 && j < board[0]
+                    .length && word.charAt(wordIndex) == board[i + 1][j]) {
 
+            } else if (word.charAt(wordIndex) == board[i][j + 1]) {
+
+            } else if (word.charAt(wordIndex) == board[i - 1][j]) {
+
+            } else if (i > -1 && i < board.length && j > 0 && j < board[0]
+                    .length && word.charAt(wordIndex) == board[i][j - 1]) {
+
+            }
+        }
         return false;
     }
 }
