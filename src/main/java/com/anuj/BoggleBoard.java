3ddd356370ef9094ecc24a4169d7be3b38f49d3b
Anuj Varma <anujva@gmail.com>
2017-11-24 01:20:37 -0800

    Almost done with boggle

--- src/main/java/com/anuj/BoggleBoard.java
+++ src/main/java/com/anuj/BoggleBoard.java
@@ -2,7 +2,13 @@ package com.anuj;
 
 public class BoggleBoard {
     public static void main(String[] args) {
+        char[][] board = new char[][]{
+                {'A', 'O', 'L'},
+                {'D', 'E', 'L'},
+                {'G', 'H', 'I'},
+        };
 
+        System.out.println(boggleSearch(board, "HELLO"));
     }
 
     public static boolean boggleSearch(char[][] board, String word) {
@@ -21,17 +27,40 @@ public class BoggleBoard {
     public static boolean searchForWord(char[][] board, String word, int i,
                                         int j, int wordIndex) {
         if (wordIndex < word.length()) {
+            boolean found = false;
             if (i > -1 && i < board.length - 1 && j > -1 && j < board[0]
                     .length && word.charAt(wordIndex) == board[i + 1][j]) {
-
-            } else if (word.charAt(wordIndex) == board[i][j + 1]) {
-
-            } else if (word.charAt(wordIndex) == board[i - 1][j]) {
-
-            } else if (i > -1 && i < board.length && j > 0 && j < board[0]
-                    .length && word.charAt(wordIndex) == board[i][j - 1]) {
-
+                found = searchForWord(board, word, i + 1, j, wordIndex + 1);
             }
+
+            if (!found) {
+                if (i > -1 && i < board.length && j > -1 && j < board[0]
+                        .length - 1 && word.charAt(wordIndex) == board[i][j +
+                        1]) {
+
+                    found = searchForWord(board, word, i, j + 1, wordIndex + 1);
+                }
+            }
+
+            if (!found) {
+                if (i > 0 && i < board.length && j > -1 && j < board[0]
+                        .length && word.charAt(wordIndex) == board[i - 1][j]) {
+                    found = searchForWord(board, word, i - 1, j, wordIndex +
+                            1);
+                }
+            }
+
+            if(!found) {
+                if (i > -1 && i < board.length && j > 0 && j < board[0]
+                        .length && word.charAt(wordIndex) == board[i][j - 1]) {
+                    found = searchForWord(board, word, i, j - 1, wordIndex +
+                            1);
+                }
+            }
+
+            return found;
+        } else if (wordIndex == word.length()) {
+            return true;
         }
         return false;
     }
