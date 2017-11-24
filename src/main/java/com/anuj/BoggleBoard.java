d16a5ab2ec3786f9697671a928919c5b01a0b8aa
Anuj Varma <anujva@gmail.com>
2017-11-23 20:12:51 -0800

    Doing boggle search now, done with steal the node

--- src/main/java/com/anuj/BoggleBoard.java
+++ src/main/java/com/anuj/BoggleBoard.java
@@ -6,6 +6,20 @@ public class BoggleBoard {
     }
 
     public static boolean boggleSearch(char[][] board, String word) {
-        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (word.charAt(0) == board[i][j]) {
+                    //begin the search
+                    boolean found = searchForWord(board, word, i, i);
+                    if (found) return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean searchForWord(char[][] board, String word, int i, int j) {
+
+        return false;
     }
 }
