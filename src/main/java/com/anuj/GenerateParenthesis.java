febe13894e7abd62f97068ab6a829d2e511085be
Anuj Varma <anuj.varma@ge.com>
2018-01-13 10:21:37 -0800

    Generate parenthesis done

---
+++ src/main/java/com/anuj/GenerateParenthesis.java
@@ -0,0 +1,28 @@
+package com.anuj;
+
+import java.util.ArrayList;
+
+public class GenerateParenthesis {
+    public static void main(String[] args) {
+        generateParenthesis(3);
+    }
+
+    public static void generateParenthesis(int n) {
+        ArrayList<String> answers = new ArrayList<>();
+        generateParenthesisSubroutine("", 0, 0, n, answers);
+        System.out.println(answers);
+    }
+
+    public static void generateParenthesisSubroutine(String s, int no, int nc, int n, ArrayList<String> answers) {
+        if (no == nc && no == n) {
+            answers.add(s);
+        }
+        if (no < n) {
+            generateParenthesisSubroutine(s.concat("("), no + 1, nc, n, answers);
+        }
+
+        if (nc < no) {
+            generateParenthesisSubroutine(s.concat(")"), no, nc + 1, n, answers);
+        }
+    }
+}
