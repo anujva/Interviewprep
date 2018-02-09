11517d5b2fcba6f3d6dcd49ae1cc30f10b03a7a2
Anuj Varma <anujva@gmail.com>
2018-02-09 07:55:00 -0800

    Skeleton for generate IP Address

---
+++ src/main/java/com/anuj/GenerateIPAddress.java
@@ -0,0 +1,15 @@
+package com.anuj;
+
+import java.util.ArrayList;
+
+public class GenerateIPAddress {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static ArrayList<String> generateIPAddrs(String s) {
+
+    return null;
+  }
+}
