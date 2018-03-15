7b6600cfb1dd1219f5348d53eb27a390610c3ebd
Anuj Varma <anujva@gmail.com>
2018-03-15 14:56:45 -0700

    Committing the correct version

--- src/main/java/com/anuj/leetcode/PickingNumbers.java
+++ src/main/java/com/anuj/leetcode/PickingNumbers.java
@@ -8,20 +8,20 @@ public class PickingNumbers {
 
     /**
      * This testcase fails.
-     * @param args
      */
     public static void main(String[] args) {
-        System.out.println(pickingNumbers(new int[]{
-            14,18,17,10,9,20,4,13,19,19,8,15,15,17,6,5,15,12,18,2,18,7,20,8,2,8,11,2,16,2,12,9,3,6,9,9,13,7,4,6,19,7,2,4,3,4,14,3,4,9,17,9,4,20,10,16,12,1,16,4,15,15,9,13,6,3,8,4,7,14,16,18, 20,11,20,14,20,12,15,4,5,10,10,20,11,18,5,20,13,4,18,1,14,3,20,19,14,2,5,13
+        System.out.println(pickingNumbers2(new int[]{
+            14, 18, 17, 10, 9, 20, 4, 13, 19, 19, 8, 15, 15, 17, 6, 5, 15, 12, 18, 2, 18, 7, 20, 8,
+            2, 8, 11, 2, 16, 2, 12, 9, 3, 6, 9, 9, 13, 7, 4, 6, 19, 7, 2, 4, 3, 4, 14, 3, 4, 9, 17,
+            9, 4, 20, 10, 16, 12, 1, 16, 4, 15, 15, 9, 13, 6, 3, 8, 4, 7, 14, 16, 18, 20, 11, 20,
+            14, 20, 12, 15, 4, 5, 10, 10, 20, 11, 18, 5, 20, 13, 4, 18, 1, 14, 3, 20, 19, 14, 2, 5,
+            13
         }));
     }
 
     /**
-     * This is a wrong implementation of picking numbers.
-     * I have kept it to remember, how this thinking didn't work
-     * and what I missed
-     * @param a
-     * @return
+     * This is a wrong implementation of picking numbers. I have kept it to remember, how this
+     * thinking didn't work and what I missed
      */
     public static int pickingNumbers(int[] a) {
         List<List<Integer>> list = new ArrayList<>();
@@ -48,6 +48,24 @@ public class PickingNumbers {
         for (int i = 0; i < list.size(); i++) {
             if (max < list.get(i).size()) {
                 max = list.get(i).size();
+            }
+        }
+        return max;
+    }
+
+    public static int pickingNumbers2(int[] a) {
+        int[] frequencies = new int[101];
+        //We are guaranteed that the values in the array are not going to be
+        for (int i = 0; i < a.length; i++) {
+            frequencies[a[i]]++;
+        }
+
+        int max = 0;
+
+        for (int i = 1; i < frequencies.length - 1; i++) {
+            int count = frequencies[i] + frequencies[i + 1];
+            if (max < count) {
+                max = count;
             }
         }
         return max;
