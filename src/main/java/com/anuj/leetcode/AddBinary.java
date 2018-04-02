2e1946b53143ee0c6402bc6404f146dc24b35360
Anuj Varma <anujva@gmail.com>
2018-04-02 11:02:56 -0700

    Updated the code in AddBinary file as well, some duplicated files around, but that's fine

--- src/main/java/com/anuj/leetcode/AddBinary.java
+++ src/main/java/com/anuj/leetcode/AddBinary.java
@@ -6,21 +6,43 @@ public class AddBinary {
     }
 
     public static String addBinary(String a, String b) {
-        String aRev = new StringBuffer(a).reverse().toString();
-        String bRev = new StringBuffer(b).reverse().toString();
+        StringBuffer binary1Buffer = new StringBuffer(a);
+        StringBuffer binary2Buffer = new StringBuffer(b);
 
-        int aRevIndex = 0;
-        int bRevIndex = 0;
+        //BinaryBuffer.reverse will reverse the buffer as well.
+        binary1Buffer.reverse();
+        binary2Buffer.reverse();
+
         int carryOver = 0;
         StringBuffer answer = new StringBuffer();
-        while (aRevIndex < aRev.length() && bRevIndex < bRev.length()) {
-            int currentSum = (aRev.charAt(aRevIndex) - '0') + (bRev.charAt
-                    (bRevIndex) - '0') + carryOver;
-            answer.append(currentSum % 2);
-            carryOver = currentSum / 2;
-            aRevIndex++;
-            bRevIndex++;
+        int lastIndexAdded = 0;
+        for (int i = 0; i < binary1Buffer.length() && i < binary2Buffer
+                .length(); i++) {
+            //the number additions can either be 0, 1, 10 or 11.
+            //so the possible integers are 0, 1, 2, 3
+            int sum = carryOver + (binary1Buffer.charAt(i) - '0') +
+                    (binary2Buffer.charAt(i) - '0');
+            carryOver = sum / 2;
+            sum = sum % 2;
+            answer.append(sum);
+            lastIndexAdded = i;
         }
+        lastIndexAdded++;
+        //Once the loop is done if we return.. we should be able to get the
+        // right answer for all equal
+        //length strings.
+        StringBuffer rest = binary1Buffer.length() > binary2Buffer.length() ?
+                binary1Buffer : binary2Buffer;
+        //whatever is the rest will need to go over the rest
+        while (lastIndexAdded < rest.length()) {
+            int sum = carryOver + (rest.charAt(lastIndexAdded) - '0');
+            carryOver = sum / 2;
+            sum = sum % 2;
+            answer.append(sum);
+            lastIndexAdded++;
+        }
+        if (carryOver != 0)
+            answer.append(carryOver);
         return answer.reverse().toString();
     }
 }
