ccea8b54e6ccb22a3e0268482fb137ee3239b73d
Anuj Varma <anujva@gmail.com>
2018-04-02 19:05:44 -0700

    Incomplete still

--- src/main/java/com/anuj/leetcode/SearchMatrix.java
+++ src/main/java/com/anuj/leetcode/SearchMatrix.java
@@ -8,10 +8,23 @@ public class SearchMatrix {
     public static boolean searchMatrix(int[][] matrix, int target) {
         //Do a binary search on the rows to pinpoint which row should
         //be searched.
-        int rowToSearch = getRowForTarget(matrix, target);
+        int rowToSearch = getRowForTarget(matrix, target, 0,
+                matrix.length - 1);
     }
 
-    private static int getRowForTarget(int[][] matrix, int target) {
-
+    private static int getRowForTarget(int[][] matrix, int target,
+                                       int startIndex, int endIndex) {
+        //What is this life if full of care we have no time to stand or stare
+        //no time to stand beneath the boughs and stare as long as sheep
+        int mid = startIndex + (endIndex - startIndex) / 2;
+        if(matrix[mid][0] <= target && target <= matrix[mid][matrix[0].length-1]) {
+            return mid;
+        } if(matrix[mid][0] < target && target > matrix[mid][matrix[0].length-1]) {
+            //target lies to the higher rows
+            return getRowForTarget(matrix, target, mid+1, endIndex);
+        } else if(matrix[mid][0] > target) {
+            //target lies to the lower rows
+            return getRowForTarget(matrix, target, startIndex, mid-1);
+        }
     }
 }
