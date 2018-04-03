9f1352e9fcae48f4aa6cdd13bc98de21970477e9
Anuj Varma <anujva@gmail.com>
2018-04-03 09:55:33 -0700

    Non decreasing array done

--- src/main/java/com/anuj/leetcode/NonDecreasingArray.java
+++ src/main/java/com/anuj/leetcode/NonDecreasingArray.java
@@ -2,10 +2,32 @@ package com.anuj.leetcode;
 
 public class NonDecreasingArray {
     public static void main(String[] args) {
-
+        int[] input = new int[]{
+                3, 4, 2, 3
+        };
+        System.out.println(checkPossibility(input));
     }
 
     public static boolean checkPossibility(int[] nums) {
+        int count = 0;
+        int indexOfInversion = -1;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                count++;
+                indexOfInversion = i;
+            }
+        }
+
+        if (count > 1)
+            return false;
+
+        if (nums[indexOfInversion - 1] <= nums[indexOfInversion + 1]) {
+            return true;
+        } else if (nums[indexOfInversion] <= nums[indexOfInversion + 2]) {
+            return true;
+        }
+
         return false;
     }
 }
