ddb58e671881b5e0eff9cb8ca21492f7171d7c51
Anuj Varma <anujva@gmail.com>
2018-04-03 20:30:26 -0700

    Incomplete still

--- src/main/java/com/anuj/geeksforgeeks/RemoveDuplicatesFromString.java
+++ src/main/java/com/anuj/geeksforgeeks/RemoveDuplicatesFromString.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 public class RemoveDuplicatesFromString {
     public static void main(String[] args) {
         System.out.println(removeDuplicatesFromString("geeksforgeeks"));
+        System.out.println(removeDuplicatesAnotherWay("geeksforgeeks"));
     }
 
     public static String removeDuplicatesFromString(String str) {
@@ -13,10 +14,28 @@ public class RemoveDuplicatesFromString {
         buffer.append(str.charAt(0));
         HashSet<Character> seenCharacters = new HashSet<>();
         seenCharacters.add(str.charAt(0));
-        for(int i=1; i < str.length(); i++) {
-            if(!seenCharacters.contains(str.charAt(i))) {
+        for (int i = 1; i < str.length(); i++) {
+            if (!seenCharacters.contains(str.charAt(i))) {
                 buffer.append(str.charAt(i));
                 seenCharacters.add(str.charAt(i));
+            }
+        }
+        return buffer.toString();
+    }
+
+    public static String removeDuplicatesAnotherWay(String str) {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(str.charAt(0));
+        for (int i = 1; i < str.length(); i++) {
+            boolean found = false;
+            for (int j = 0; j < i; j++) {
+                if (str.charAt(j) == str.charAt(i)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                buffer.append(str.charAt(i));
             }
         }
         return buffer.toString();
