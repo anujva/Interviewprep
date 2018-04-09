3bd186e86672d40d8a0f1fe694eb77766a4a47c7
Anuj Varma <anujva@gmail.com>
2018-04-08 20:26:27 -0700

    switching to office

--- src/main/java/com/anuj/leetcode/RemoveDuplicatesFromSortedListII.java
+++ src/main/java/com/anuj/leetcode/RemoveDuplicatesFromSortedListII.java
@@ -7,26 +7,59 @@ public class RemoveDuplicatesFromSortedListII {
     }
 
     public static ListNode deleteDuplicates(ListNode head) {
-        ListNode prevDup = null;
-        ListNode current = head;
-
-        while (current != null) {
-            if (current.next != null) {
-                if (current.data == current.next.data) {
-                    current = current.next;
-                } else {
-                    if (prevDup != null) {
-
+        ListNode prevCurrentNode = null;
+        ListNode currentNode = head;
+        boolean duplicates = false;
+        //how to keep track of the head;
+        //the head of the list is going to be when the first time
+        //prevCurrentNode is set to not null value;
+        while (currentNode != null) {
+            if (currentNode.next != null) {
+                if (currentNode.data != currentNode.next.data) {
+                    //if the current data is not equal to next data.
+                    //we have to see if the case is:
+                    // 4 -> 5 or 4-> 5 -> 5 -> 6
+                    //or 5-> 5-> 6
+                    if (!duplicates) {
+                        if (prevCurrentNode == null) {
+                            head = currentNode;
+                        }
+                        prevCurrentNode = currentNode;
+                        currentNode = currentNode.next;
                     } else {
-                        prevDup = current;
-                        current = current.next;
+                        if (prevCurrentNode == null) {
+                            //there are duplicates and prevCurrentNode is still null
+                            //this would mean that we have only met duplicates
+                            //till now and not enough infomation to set the prevCurrentNode
+                            currentNode = currentNode.next;
+                        } else {
+                            prevCurrentNode.next = currentNode.next;
+                            currentNode.next = null;
+                            currentNode = prevCurrentNode.next;
+                        }
+                        duplicates = false;
                     }
+                } else {
+                    duplicates = true;
+                    currentNode = currentNode.next;
                 }
             } else {
-                if (prevDup != null) {
-
+                //currentNode is pointing to the end of the list right now
+                if (duplicates) {
+                    if (prevCurrentNode == null) {
+                        return null;
+                    } else {
+                        prevCurrentNode.next = null;
+                    }
+                } else {
+                    if (prevCurrentNode == null) {
+                        prevCurrentNode = currentNode;
+                        head = currentNode;
+                    }
                 }
+                currentNode = currentNode.next;
             }
         }
+        return head;
     }
 }
