2f92f1a862385319665e886a3a4d7f96815c5828
Anuj Varma <anujva@gmail.com>
2019-02-23 18:59:49 -0800

    Still working on this

--- leetcode/KthPermutation.go
+++ leetcode/KthPermutation.go
@@ -1,6 +1,8 @@
 package main
 
-import "os"
+import (
+	"fmt"
+)
 
 func getSliceOfIntegers(n int) []int {
 	sliceOfIntegers := make([]int, 0)
@@ -11,10 +13,18 @@ func getSliceOfIntegers(n int) []int {
 }
 
 // KthPermutationSequence does the Kth Permutations
-func KthPermutationSequence(int n) {
+func KthPermutationSequence(n int) {
 	sliceOfIntegers := getSliceOfIntegers(n)
+	printSliceOfIntegers(sliceOfIntegers)
+}
+
+func printSliceOfIntegers(si []int) {
+	for _, val := range si {
+		fmt.Printf("%v ", val)
+	}
+	fmt.Println()
 }
 
 func main() {
-	args := os.Args[1:]
+	KthPermutationSequence(4)
 }
