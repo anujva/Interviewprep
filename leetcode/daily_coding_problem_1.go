165deaa0f23013fad587fbf7230ce1a36c14132b
Anuj Varma <anujva@gmail.com>
2019-03-16 15:36:49 -0700

    Add solution for the first one

--- leetcode/daily_coding_problem_1.go
+++ leetcode/daily_coding_problem_1.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func sum(arr []int, k int) bool {
+	m := make(map[int]bool)
+
+	for i := 0; i < len(arr); i++ {
+		//check before putting the value in to the map
+		if m[k-arr[i]] {
+			return true
+		}
+		m[arr[i]] = true
+	}
+	return false
+}
+
+func main() {
+	arr := []int{10, 15, 3, 7}
+	fmt.Println(sum(arr, 10))
 }
