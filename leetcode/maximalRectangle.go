dd970be2332e2ab3aa577c98e960d44260e52745
Anuj Varma <anujva@gmail.com>
2019-03-30 01:32:37 -0700

    Incomplete work

--- leetcode/maximalRectangle.go
+++ leetcode/maximalRectangle.go
@@ -5,9 +5,21 @@ package main
 func maximalRectangle(matrix [][]byte) int {
 	// The idea would be to find the largest rectangle
 	// from every starting index
+	largestRectangle := 0
 	for i := 0; i < len(matrix); i++ {
 		for j := 0; j < len(matrix[0]); j++ {
 			// start from this index and try to find the largest rectangle
+			k := getLargestRectangeAtIndex(i, j, matrix)
+			if largestRectangle < k {
+				largestRectangle = k
+			}
 		}
+	}
+	return largestRectangle
+}
+
+func largestRectangleAtIndex(i, j int, matrix [][]byte) int {
+	for row := i; row < len(matrix); row++ {
+
 	}
 }
