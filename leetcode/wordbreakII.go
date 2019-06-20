be120e232561191f046abd3b00c8bfa5ed7d0c02
Anuj Varma <anujva@gmail.com>
2019-06-19 18:36:36 -0700

    Memoize so that answer gets accepted

--- leetcode/wordbreakII.go
+++ leetcode/wordbreakII.go
@@ -2,23 +2,23 @@ package main
 
 import "fmt"
 
-var mapOfWords map[string]bool
-
 func wordBreak(s string, wordDict []string) []string {
 	if len(s) == 0 {
 		return []string{}
 	}
 
-	if mapOfWords == nil {
-		mapOfWords = make(map[string]bool)
-		for _, word := range wordDict {
-			mapOfWords[word] = true
-		}
+	mapOfWords := make(map[string]bool)
+	for _, word := range wordDict {
+		mapOfWords[word] = true
 	}
-	return wordBreakSubroutine(s, mapOfWords)
+	memoize := make(map[string][]string)
+	return wordBreakSubroutine(s, mapOfWords, memoize)
 }
 
-func wordBreakSubroutine(s string, mapOfWords map[string]bool) []string {
+func wordBreakSubroutine(s string, mapOfWords map[string]bool, memoize map[string][]string) []string {
+	if sens, ok := memoize[s]; ok {
+		return sens
+	}
 	answer := []string{}
 	if mapOfWords[s] {
 		answer = append(answer, s)
@@ -26,7 +26,7 @@ func wordBreakSubroutine(s string, mapOfWords map[string]bool) []string {
 	for i := 1; i <= len(s); i++ {
 		sub := s[0:i]
 		if mapOfWords[sub] {
-			sentences := wordBreakSubroutine(s[i:], mapOfWords)
+			sentences := wordBreakSubroutine(s[i:], mapOfWords, memoize)
 			// sentences should be non empty otherwise it will just return
 			// and not add anything to the array
 			for _, sentence := range sentences {
@@ -34,12 +34,13 @@ func wordBreakSubroutine(s string, mapOfWords map[string]bool) []string {
 			}
 		}
 	}
+	memoize[s] = answer
 	return answer
 }
 
 func main() {
-	//answers := wordBreak("pineapplepenapple", []string{"apple", "pen", "applepen", "pine", "pineapple"})
-	answers := wordBreak("catsanddogs", []string{"cats", "cat", "and", "sand", "dogs"})
+	answers := wordBreak("pineapplepenapple", []string{"apple", "pen", "applepen", "pine", "pineapple"})
+	//answers := wordBreak("catsanddogs", []string{"cats", "cat", "and", "sand", "dogs"})
 	fmt.Println(len(answers))
 	for _, answer := range answers {
 		fmt.Println(answer)
