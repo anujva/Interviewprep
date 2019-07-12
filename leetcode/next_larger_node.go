93e26c094dd64f4a8b1a314d8ae2ca98ea37dcdd
Anuj Varma <anujva@gmail.com>
2019-07-11 23:53:21 -0700

    O(n^2) implementation of this

--- leetcode/next_larger_node.go
+++ leetcode/next_larger_node.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -7,4 +9,25 @@ type ListNode struct {
 
 func nextLargerNodes(head *ListNode) []int {
 	answer := make([]int, 0)
+	for temp := head; temp != nil; temp = temp.Next {
+		t := temp.Next
+		added := false
+		for t != nil {
+			if t.Val > temp.Val {
+				answer = append(answer, t.Val)
+				added = true
+				break
+			}
+			t = t.Next
+		}
+		if !added {
+			answer = append(answer, 0)
+		}
+	}
+
+	return answer
+}
+
+func main() {
+	fmt.Println(nextLargerNodes(head))
 }
